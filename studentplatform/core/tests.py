6561a908973f33df563ff0b09ad13bb8a28bf0de
HP <aleheeb1@gmail.com>
2024-03-08 01:37:04 +0200

    i enetered the tests of the parent sighnup

--- studentplatform/core/tests.py
+++ studentplatform/core/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+
+# test for the parent
+def test_template_used(self):
+    response = self.client.get(reverse('signupparent'))
+    self.assertTemplateUsed(response, 'signupparent.html')
+
+def test_form_class(self):
+    response = self.client.get(reverse('signupparent'))
+    self.assertIsInstance(response.context['form'], SignupParentForm)ignupParentForm)
+
+
+def test_model_used(self):
+    self.assertEqual(SignupParent.model, User)
+
+
+def test_view_success_status_code(self):
+    response = self.client.get(reverse('signupparent'))
+    self.assertEqual(response.status_code, 200)
